bc257e5820286415637bf33fb0de04c0027619cf
CorwinWillms <willmsca@dukes.jmu.edu>
2021-11-19 15:46:55 -0500

    timer_callback and map_callback

--- random_nav/random_nav.py
+++ random_nav/random_nav.py
@@ -25,7 +25,7 @@ repeat forever:
 """
 
 class RandomNavNode(rclpy.node.Node):
-    def __init__(self, x, y, theta, timeout):
+    def __init__(self):
         super().__init__('nav_demo')
 
         self.create_timer(.1, self.timer_callback)
@@ -38,11 +38,38 @@ class RandomNavNode(rclpy.node.Node):
         self.ac.wait_for_server()
         self.get_logger().info("NAVIGATION SERVER AVAILABLE...")
 
-        self.goal = NavigateToPose.Goal()
+        self.goal = None
         self.goal.pose.header.frame_id = 'map'  # SEEMS TO BE IGNORED!
         #find random values to assign instead
-        self.goal.pose.pose.position.x = x 
-        self.goal.pose.pose.position.y = y
+        
+        self.map = None
+
+    def timer_callback(self):
+        while True:
+            while self.goal is None:
+                x = randrange(0.0, self.map.width)
+                y = randrange(0.0, self.map.height)
+                val = self.map.get_cell(x, y)
+                if val == 0:
+                    self.goal = NavigateToPose.Goal()
+                    x = self.goal.pose.pose.position.x
+                    y = self.goal.pose.pose.position.y
+            self.goal_future = self.ac.send_goal_async(self.goal)
+            while not self.goal_future.done():
+                pass 
+            self.goal = None
+            
+
+                
+    
+    def map_callback(self, map_msg):
+        """Process the map message.  This doesn't really do anything useful, it is
+        purely intended as an illustration of the Map class.
+
+        """
+        if self.map is None:  # No need to do this every time map is published.
+
+            self.map = map_utils.Map(map_msg)
 
 
 def main():
